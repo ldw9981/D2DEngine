d1871d0a1b854003e6fc0c66d511e8cd499095b6
LeeDongWon <ldw9981@gmail.com>
2024-01-17 00:55:28 +0900

    라인 제거

--- D2DRenderer/Factory.h
+++ D2DRenderer/Factory.h
@@ -5,7 +5,6 @@
 #include <map>
 #include <functional> // For std::function
 
-
 class GameObject;
 
 // CPP에서 사용합니다.
@@ -13,12 +12,10 @@ class GameObject;
     GameObject* CreateGameObject##GameObjectType() { return new GameObjectType(); } \
     bool Registered##GameObjectType = Factory::RegisterGameObject(#GameObjectType, CreateGameObject##GameObjectType);
 
-
 class Factory
 {
 public:
 	using GameObjectCreator = std::function<GameObject* ()>;
-
 	static GameObject* CreateGameObject(const std::string& GameObjectType) {
 		auto it = GetRegistry().find(GameObjectType);
 		if (it != GetRegistry().end()) {
@@ -26,13 +23,10 @@ public:
 		}
 		return nullptr;
 	}
-
 	static bool RegisterGameObject(const std::string& GameObjectType, GameObjectCreator creator) {
 		GetRegistry()[GameObjectType] = creator;
 		return true;
 	}
-
-
 	static std::map<std::string, GameObjectCreator>& GetRegistry() {
 		static std::map<std::string, GameObjectCreator> registry;
 		return registry;
